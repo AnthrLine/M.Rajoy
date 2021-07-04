3481a6bb99a9521bbaafdda27fcb9731c33f584b
Anthr. Line <64476467+AnthrLine@users.noreply.github.com>
2021-07-04 13:15:09 +0200

    New commands on moderacio.py

--- cogs/moderacio.py
+++ cogs/moderacio.py
@@ -1,6 +1,6 @@
 import discord
 import sys
-from discord.ext import commands
+from discord.ext import *
 
 class principals(commands.Cog):
 
@@ -20,12 +20,27 @@ class principals(commands.Cog):
 	@commands.command()
 	@commands.has_permissions(administrator=True)
 	async def expulsa(self, ctx, member: discord.Member, *, why=None):
+		await ctx.send('Expulsando...')
 		await member.kick(reason=why)
 
 		embed = discord.Embed(title="Expulsión", colour=discord.Colour(0x42ff68))
 
 		embed.add_field(name='Detalles de la expulsión:', value=f'**{member} ha sido expulsado por {ctx.author}**')
 
+		await ctx.channel.send(embed=embed)
+
+	@commands.command(pass_context = True)
+	@commands.has_permissions(manage_channels=True)
+	async def mutear(self, ctx, member: discord.Member, role: discord.Role):
+		await member.add_roles(role)
+		embed=discord.Embed(title="Usuario muteado", description=f"**{member}** ha sido muteado por **{ctx.author}**!", color=0xff00f6)
+		await ctx.channel.send(embed=embed)
+
+	@commands.command(pass_context = True)
+	@commands.has_permissions(manage_channels=True)
+	async def desmutear(self, ctx, member: discord.Member, role: discord.Role):
+		await member.remove_roles(role)
+		embed=discord.Embed(title="Usuario desmuteado", description=f"**{member}** ha sido desmuteado por **{ctx.author}**!", color=0xff00f6)
 		await ctx.channel.send(embed=embed)
 
 def setup(client):
